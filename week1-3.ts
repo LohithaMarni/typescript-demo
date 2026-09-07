864820055ff87ce94d5db0b447b860ae7f5bfd5d
Lohitha Marni <lohithamarni@gmail.com>
2026-09-07 13:11:51 +0530

    folders

--- week1-3.ts
+++
@@ -1,9 +0,0 @@
-let movieTitle: string = "RRR";
-let releaseYear: number = 2022;
-let isBlockbuster: boolean = true;
-
-function getMovieInfo(title: string, year: number): string {
-    return `${title} was released in ${year}.`;
-}
-
-let leadActors: string[] = ["N.T.R", "Ram Charan", "Alia Bhatt", "Ajay Devgn"];
